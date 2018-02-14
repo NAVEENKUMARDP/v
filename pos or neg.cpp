97344e3474833a6f6f1db4aae7b19f3a4a936e8c
vijhay <36472237+vijhay@users.noreply.github.com>
2018-02-14 15:36:24 +0530

    Create pos or neg.cpp

---
+++ pos or neg.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+
+void main()
+{
+ 
+ int number;
+ cout<<"Enter a number : ";
+ cin>>number;
+ if(number>0)
+   cout<<"Number is Positive";
+ else if(number<0) 
+   cout<<"Number is Negative";
+ else
+   cout<<"Number is Zero";
+ 
+}
