4584bd9aca80e287d57bf0753c83d4f719f85fba
vijhay <36472237+vijhay@users.noreply.github.com>
2018-02-15 10:27:32 +0530

    Update odd or even.cpp

--- odd or even.cpp
+++ odd or even.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main() {
 	int num;
-	cout<<"Enter the Number to check\n";
+	cout<<"Enter the Number to check";
 	cin>>num;
 	if(num!=0)
 	{
 	if(num%2==0)
 	{
-		cout<<"The Number odd";
+	cout<<"The Number odd";
 	}
 	else
 	{
-		cout<<"The number is even";
+	cout<<"The number is even";
 	}
 	}
 	else
 	{
-		cout<<"Enter valied positive integer";
+	cout<<"Enter valied positive integer";
 	}
 	return 0;
 }
